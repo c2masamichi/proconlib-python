0d2c8b7124fa14c6361ec4be6480cf1a812fc89a
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2020-05-21 22:59:11 +0900

    use pytest

--- number/test_prog.py
+++ number/test_prog.py
@@ -1,22 +1,18 @@
-import unittest
+import pytest
 
 import prog
 
 
-class TestClient(unittest.TestCase):
-
-    def test_is_prime(self):
-        parameters = (
-            (1, False),
-            (2, True),
-            (4, False),
-            (5, True),
-            (71, True),
-            (91, False),
-        )
-        for x, expected in parameters:
-            with self.subTest(x=x, expected=expected):
-                self.assertEqual(prog.is_prime(x), expected)
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+@pytest.mark.parametrize(
+    ('x', 'expected'),
+    (
+        (1, False),
+        (2, True),
+        (4, False),
+        (5, True),
+        (71, True),
+        (91, False),
+    ),
+)
+def test_is_prime(x, expected):
+    assert prog.is_prime(x) == expected
